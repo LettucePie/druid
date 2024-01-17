f10caab43dbd0d61ec52b0faa65904a7b3cc818f
Matthew LettucePie <thelettucepie@gmail.com>
2024-01-17 11:35:30 -0500

    found desired vector manipulation - slide

--- core/player.gd
+++ core/player.gd
@@ -4,37 +4,62 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var current_cam : Camera3D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+func _ready():
+	current_cam = get_viewport().get_camera_3d()
+
+
 func _physics_process(delta):
+	current_cam = get_viewport().get_camera_3d()
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity -= get_up_direction() * (gravity * delta)
+#		velocity.y -= gravity * delta
+	else:
+		## Check if climbing
+		if get_slide_collision_count() > 0:
+			var col : KinematicCollision3D = get_slide_collision(0)
+		#	if col.get_collider().is_in_group("climbable"):
+			set_up_direction(col.get_normal())
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity = get_up_direction() * JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var input_dir = Vector3(
 		Input.get_axis("stick_l_x-", "stick_l_x+"), 
 		0, 
-		Input.get_axis("stick_l_y-", "stick_l_y+"))
-	print(input_dir)
+		Input.get_axis("stick_l_y-", "stick_l_y+")).normalized()
+	var stop : bool = input_dir == Vector3.ZERO
 	
-#	rotate_y(delta * (input_dir.x * - 1))
-	var direction_orient : Basis = get_viewport().get_camera_3d().transform.basis
-	var direction = (direction_orient * input_dir).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	## Assign movement direction to Camera Orientation
+#	## Basis Method
+#	var direction_orient : Basis = current_cam.transform.basis
+#	var direction = (direction_orient * input_dir).normalized()
+#	## Note
+#	## Multiplying input by Camera.transform.basis causes direction.y to reflect
+#	## the up and down tilt of the Camera.
+	
+	## Angle Method
+	var direction = input_dir.rotated(Vector3.UP, current_cam.rotation.y)
+	
+	## Transform direction perpindicular to get_up_direction()
+	## How? I don't know if it's cross, slide, bounce, reflect...
+#	direction = direction.slide(get_up_direction())
+	direction = direction.cross(get_up_direction())
+	
+#	print(direction)
+	if stop:
+		velocity.lerp(Vector3.ZERO, delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity = direction * SPEED
 
 	move_and_slide()
 
