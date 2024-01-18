621f97d99626376a452c67c9422b7323234c091b
Matthew LettucePie <thelettucepie@gmail.com>
2024-01-18 11:48:47 -0500

    started adding floor snapping

--- core/player.gd
+++ core/player.gd
@@ -13,12 +13,15 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	current_cam = get_viewport().get_camera_3d()
-	motion_mode = 1
+	motion_mode = 0
 	floor_max_angle = PI
 
 
-func update_up():
-	pass
+func update_up(up : Vector3):
+	previous_normal = get_up_direction()
+	set_up_direction(up)
+	$magnet_ray.target_position = up * -1.0
+	print("Update Up Direction ", get_up_direction())
 
 
 func _physics_process(delta):
@@ -26,26 +29,32 @@ func _physics_process(delta):
 	var col = get_last_slide_collision()
 	if col != null:
 		if col.get_normal() != get_up_direction():
-			previous_normal = get_up_direction()
-			set_up_direction(col.get_normal())
-			print("Update Up Direction ", get_up_direction())
+			update_up(col.get_normal())
 	var input_h = Input.get_axis("stick_l_x-", "stick_l_x+")
 	var input_v = Input.get_axis("stick_l_y-", "stick_l_y+")
 	var input_vec : Vector3 = Vector3(input_h, 0, input_v)
 	var direction = input_vec.rotated(Vector3.UP, current_cam.rotation.y).normalized()
-	if get_up_direction() != Vector3.UP:
+	var floor_angle : float = Vector3.UP.angle_to(get_up_direction())
+#	print(floor_angle, " / ", (PI / 6))
+	if floor_angle > (PI / 6):
 #		direction = input_vec.slide(get_up_direction())
 		direction = direction.rotated(
 			Vector3.UP.cross(get_up_direction()).normalized(),
-			Vector3.UP.angle_to(get_up_direction())
+			floor_angle
 		).normalized()
 	if is_on_floor() or is_on_wall():
-		print("Floor: ", is_on_floor(), " Wall: ", is_on_wall())
+#		print("Floor: ", is_on_floor(), " Wall: ", is_on_wall())
 		velocity = direction * SPEED
 	else:
-		print("falling")
-		set_up_direction(Vector3.UP)
-		velocity.y -= gravity * delta
+#		print("falling")
+		if $magnet_ray.is_colliding() and floor_angle > (PI / 6):
+#			print("pull to wall")
+			velocity -= get_up_direction() * (gravity * delta)
+		else:
+#			print("disconnected from wall, traditional fall")
+			if get_up_direction() != Vector3.UP:
+				update_up(Vector3.UP)
+			velocity.y -= gravity * delta
 	move_and_slide()
 
 
